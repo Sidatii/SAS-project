e4fc94b314d25d8e7bf1d93630f5c3c27d93047b
sidatii <sidatnouhi@gmail.com>
2022-09-18 19:18:09 +0100

    Version 0.2 -Supprimer added

--- SAS-Pojectt.c
+++ SAS-Pojectt.c
@@ -27,7 +27,7 @@ void ajoute(){
 start:
 int n;
 printf("************************Ajoutez vos produits************************\n\n ");
-printf("Veuillez saisir le nombre des produits à ajouter: ");
+printf("Veuillez saisir le nombre des produits a ajouter: ");
 scanf("%d", &n);
 printf("Veuillez saisir les infos de votre produit: ");
 int temp = nb_produit;
@@ -49,24 +49,54 @@ int temp = nb_produit;
     system("cls");
     printf("..................Votre produit est bien enregistre..................");
     }
-
-    printf("\n1. Afficher la liste des produits\n2. Retour au menu principale");
-    scanf("%d",&choix);
-    system("cls");
+}
 
 
+
+
+void supprimer(){
+int del_num,del_code,index;
+start:
+int n;
+printf("************************Supprimez un ou plusieurs produits************************\n\n ");
+printf("Veuillez saisir le nombre des produits a supprimer: ");
+scanf("%d", &n);
+
+for(i=0; i<n; i++){
+printf("Veuillez inserer le code du produit a supprimer n %d: ",i+1);
+    scanf("%d",&del_code);fflush(stdin);
+ for (j=0; j<nb_produit;j++){
+
+    if(s[j].codee==del_code){
+            index=j;
+    }
+    if(index != -1){
+    for(j=index; j<nb_produit;j++)
+        s[i]= s[i+1];
+        nb_produit--;
     }
 
+    printf("..................Votre produit est supprime..................\n\n");
+    }
+}
+printf("Votre nouvelle liste s'affiche comme suit: \n");
+for (j=0; j<nb_produit;j++)
+    printf("\n%d \t %s \t %d \t %f \t %f\n",s[j].codee,s[j].inf.nom,s[j].inf.qty,s[j].inf.price,s[j].inf.price_TTC);
 
 
+}
 
 void list_produits(){
-    int afficher;
-    /*printf("Veuillez saisir le nombre des lignes a afficher: ");
-    scanf("%d", &afficher);*/
-    for (j=0; j<nb_produit;j++)
-    printf("\n%d \t %s \t %d \t %f \n",s[j].codee,s[j].inf.nom,s[j].inf.qty,s[j].inf.price,s[j].inf.price_TTC);
+
+    for (j=0; j<nb_produit;j++){
+    printf("\n%d \t %s \t %d \t %f \t %f\n",s[j].codee,s[j].inf.nom,s[j].inf.qty,s[j].inf.price,s[j].inf.price_TTC);
+    }
+
 }
+
+
+
+
 
 
 ///
@@ -75,8 +105,8 @@ int main () {
     start1:
     system("cls");
     printf("*******************************************************************************************************************\n");
-    printf("                                   The NOUHI Pharmacy management system\n");
-    printf("                               Your application to better manage your pharmacy \n");
+    printf("                           ||      The NOUHI Pharmacy management system         ||\n");
+    printf("                           ||  Your application to better manage your pharmacy  ||\n");
     printf("*******************************************************************************************************************\n\n");
     printf("Bienvenue!! Que est ce que vous voulez faire?\n\n");
     start:
@@ -101,16 +131,67 @@ int main () {
 
     switch(choix){
         case 1:
+            aj:
+            system("cls");
             ajoute();
+            printf("\n1. Afficher la liste des produits\n2. Retour au menu principale\n");
+            printf("-------> ");
+            scanf("%d",&choix);
+            system("cls");
             switch (choix){
                 case 1:
                     list_produits();
-                    break;
+                    printf("\n1. Ajouter un ou plusieurs produits\n2. Supprimer un ou plusieurs produits\n3. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch(choix){
+                            case 1: goto aj;
+                            case 2: goto supp;
+                            case 3: goto start1;
+                            }
+
                 case 2:
                     goto start1;
                     break;
-        //case 2:
-}
+                    }
+        case 2:
+            supp:
+            supprimer();
+            printf("\n1. Ajouter un ou plusieurs produits\n2. Supprimer un ou plusieurs produits\n3. Retour au menu principale\n");
+            printf("-------> ");
+            scanf("%d",&choix);
+            system("cls");
+                    switch(choix){
+                        case 1:
+                            goto aj;
+                        case 2:
+                            goto supp;
+                        case 3:
+                            goto start1;
+                    }
+
+        case 3: list:
+            system("cls");
+            list_produits();
+            printf("\n1. Ajouter un ou plusieurs produits\n2. Retour au menu principale\n");
+            printf("-------> ");
+            scanf("%d",&choix);
+            system("cls");
+                switch (choix){
+                    case 1:
+                        goto aj;
+                    case 2:
+                        goto start1;
+
+                }
+    }
+
+
+
+
+
+
 ///Ajouter un ou plusieurs produits:
 
 ///Supprimer un ou plusieurs produits:
@@ -127,4 +208,4 @@ int main () {
 return 0;
 
 }
-}
+
