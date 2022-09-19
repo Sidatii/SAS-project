3513bbe70def990f461ccddbd1509a928ffb18eb
sidatii <sidatnouhi@gmail.com>
2022-09-19 17:37:38 +0100

    Version 0.4

--- SAS-Pojectt.c
+++ SAS-Pojectt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int nb_produit = 0;
+int compteur = 0;
 
 typedef struct{
 
@@ -17,15 +18,33 @@ infos inf;
 
 }products;
 
+typedef struct{
+
+
+char nom[14];
+int qty;
+float prix;
+float Total_TTC;
+
+}info;
+
+typedef struct{
+
+int codee;
+info inf;
+
+}ventes;
+
 products s[100];
+ventes t[100];
 int j, n,choix;
 char check[100];
-
 int i,j;
-void ajoute(){
 
-start:
+void ajoute(){
 int n;
+start:
+
 printf("************************Ajoutez vos produits************************\n\n ");
 printf("Veuillez saisir le nombre des produits a ajouter: ");
 scanf("%d", &n);
@@ -53,8 +72,9 @@ int temp = nb_produit;
 
 void supprimer(){
 int del_num,del_code,index;
-start:
 int n;
+start:
+
 printf("************************Supprimez un ou plusieurs produits************************\n\n ");
 printf("Veuillez saisir le nombre des produits a supprimer: ");
 scanf("%d", &n);
@@ -91,6 +111,121 @@ void list_produits(){
 
 }
 
+void reduire_qte(){
+
+int reduire_num,reduire_code,redu_qte,index;
+int n;
+printf("************************Reduire la quantite d un ou plusieurs produits************************\n\n ");
+printf("Veuillez saisir le nombre des produits a mettre a jour: ");
+scanf("%d", &n);
+
+for(i=0; i<n; i++){
+    printf("Veuillez inserer le code du produit a mettre a jour n %d: ",i+1);
+    scanf("%d",&reduire_code);fflush(stdin);
+    printf("Veuillez inserer la quantite a deduire: ");
+    scanf("%d",&redu_qte);fflush(stdin);
+
+        for(int m=0; m<nb_produit;m++)
+            if(s[m].codee==reduire_code){
+                index=m;
+            }
+        s[index].inf.qty -= redu_qte;
+        t[compteur].codee = s[index].codee;
+        strcpy(t[compteur].inf.nom,s[index].inf.nom);
+        t[compteur].inf.qty = redu_qte;
+        t[compteur].inf.prix = s[index].inf.price;
+        t[compteur].inf.Total_TTC = s[compteur].inf.price*s[compteur].inf.qty*1.15;
+        compteur++;
+
+
+
+    printf("..................La quantite produit est mis a jour..................\n\n");
+
+}
+printf("Votre nouvelle liste s'affiche comme suit: \n\n");
+printf("Code\tNom\tQte\tPrix HT\t  Prix TTC");
+for (int k=0; k<nb_produit;k++)
+    printf("\n%d \t %s \t %d \t %f \t %f\n",s[k].codee,s[k].inf.nom,s[k].inf.qty,s[k].inf.price,s[k].inf.price_TTC);
+
+}
+
+void augmente_qte(){
+
+int augmente_num,augmente_code,augm_qte,index;
+int n;
+printf("************************Alimenter la quantite d un ou plusieurs produits************************\n\n ");
+printf("Veuillez saisir le nombre des produits a mettre a jour: ");
+scanf("%d", &n);
+
+for(i=0; i<n; i++){
+    printf("Veuillez inserer le code du produit a mettre a jour n %d: ",i+1);
+    scanf("%d",&augmente_code);fflush(stdin);
+    printf("Veuillez inserer la quantite a deduire: ");
+    scanf("%d",&augm_qte);fflush(stdin);
+    for (j=0; j<nb_produit;j++){
+
+        for(int m=0; m<nb_produit;m++){
+            if(s[m].codee==augmente_code){
+                index=m;
+            }
+                s[index].inf.qty += augm_qte;
+
+        }
+    }
+    printf("..................Votre quantite produit est mis a jour..................\n\n");
+
+}
+printf("Votre nouvelle liste s'affiche comme suit: \n\n");
+printf("Code\tNom\tQte\tPrix HT\t  Prix TTC");
+for (int k=0; k<nb_produit;k++)
+    printf("\n%d \t %s \t %d \t %f \t %f\n",s[k].codee,s[k].inf.nom,s[k].inf.qty,s[k].inf.price,s[k].inf.price_TTC);
+
+}
+
+void Ascendant_par_nom(){
+
+for(i=0; i<nb_produit;i++){
+    if(strcmp(s[i].inf.nom,s[i+1].inf.nom)==1){
+        products temp = s[i];
+        s[i] = s[i+1];
+        s[i+1] = temp;
+    }
+
+}
+printf("Votre nouvelle liste s'affiche comme suit: \n\n");
+printf("Code\tNom\tQte\tPrix HT\t  Prix TTC");
+for (int k=0; k<nb_produit;k++)
+    printf("\n%d \t %s \t %d \t %f \t %f\n",s[k].codee,s[k].inf.nom,s[k].inf.qty,s[k].inf.price,s[k].inf.price_TTC);
+
+}
+
+void Descendant_par_nom(){
+
+for(i=0; i<nb_produit;i++){
+    if(s[i].inf.price < s[i+1].inf.price){
+        products temp = s[i];
+        s[i] = s[i+1];
+        s[i+1] = temp;
+    }
+
+}
+printf("Votre nouvelle liste s'affiche comme suit: \n\n");
+printf("Code\tNom\tQte\tPrix HT\t  Prix TTC");
+for (int k=0; k<nb_produit;k++)
+    printf("\n%d \t %s \t %d \t %f \t %f\n",s[k].codee,s[k].inf.nom,s[k].inf.qty,s[k].inf.price,s[k].inf.price_TTC);
+
+}
+
+void Journal_ventes(){
+    printf("\nCode\tNom\tQte\tPrix\tTotal TTC\n");
+    for (j=0; j<compteur;j++){
+    printf("\n%d \t %s \t %d \t %f \t %f\n",t[j].codee,t[j].inf.nom,t[j].inf.qty,t[j].inf.prix,t[j].inf.Total_TTC);
+    }
+
+}
+
+
+
 int main () {
 
     start1:
@@ -100,7 +235,7 @@ int main () {
     printf("                           ||  Your application to better manage your pharmacy  ||\n");
     printf("*******************************************************************************************************************\n\n");
     printf("Bienvenue!! Que est ce que vous voulez faire?\n\n");
-    start:
+    invalid:
     printf("________________________________\n");
     printf("1 : Ajouter un ou plusieurs produits \n");
     printf("2 : Supprimer un ou plusieurs produits\n");
@@ -116,7 +251,7 @@ int main () {
         fgets(check, 100, stdin);
         system("cls");
         printf("Saisie invalid!!! \nVeuillez saisir un choix valid.\n ");
-        goto start;
+        goto invalid;
         }
 
 
@@ -140,12 +275,11 @@ int main () {
                         switch(choix){
                             case 1: goto aj;
                             case 2: goto supp;
-                            case 3: goto start1;
+                            default: goto start1;
                             }
 
                 case 2:
                     goto start1;
-                    break;
                     }
 ///Supprimer un ou plusieurs produits:
         case 2:
@@ -160,26 +294,125 @@ int main () {
                             goto aj;
                         case 2:
                             goto supp;
-                        case 3:
+                        default:
                             goto start1;
                     }
 ///Liste des produits
-        case 3: list:
-            system("cls");
-            list_produits();
-            printf("\n1. Ajouter un ou plusieurs produits\n2. Retour au menu principale\n");
+        case 3:
+            printf("\n1. Afficher la list des produits\n2. Afficher la list triee ascendante par nom\n3. Afficher la list triee descendante par prix\n4. Retour au menu principale\n");
+            printf("-------> ");
+            scanf("%d",&choix);
+            switch (choix){
+                case 1:
+                    list:
+                    system("cls");
+                    list_produits();
+                    printf("\n1. Ajouter un ou plusieurs produits\n2. Supprimer un ou plusieurs produits\n3. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch (choix){
+                            case 1:
+                                goto aj;
+                            case 2:
+                                goto supp;
+                            default:
+                                goto start1;
+                            }
+
+                case 2:
+                    system("cls");
+                    Ascendant_par_nom();
+                    printf("\n1. Ajouter un ou plusieurs produits\n2. Supprimer un ou plusieurs produits\n3. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch (choix){
+                            case 1:
+                                goto aj;
+                            case 2:
+                                goto supp;
+                            default:
+                                goto start1;
+                            }
+                case 3:
+                    /*system("cls");
+                    Descendant_par_prix();
+                    printf("\n1. Ajouter un ou plusieurs produits\n2. Supprimer un ou plusieurs produits\n3. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch (choix){
+                            case 1:
+                                goto aj;
+                            case 2:
+                                goto supp;
+                            default:
+                                goto start1;
+                            }*/
+                default:
+                    system("cls");
+                    Ascendant_par_nom();
+
+
+
+            }
+///Gestion vente\achat
+        case 4:
+            printf("\n1. Reduire la quantite d un ou plusieurs produit\n2. Alimenter la quantite d un ou plusieurs produit\n3. Retour au menu principale\n");
             printf("-------> ");
             scanf("%d",&choix);
             system("cls");
-                switch (choix){
-                    case 1:
-                        goto aj;
-                    case 2:
-                        goto start1;
+            switch(choix){
+                case 1:
+                    reduire:
+                    reduire_qte();
+                    printf("\n1. Reduire la quantite d un ou plusieurs produit\n2. Augmenter la quantite d un ou plusieurs produit\n3. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch (choix){
+                            case 1:
+                                goto reduire;
+                            case 2:
+                                goto augmente;
+                            default:
+                                goto start1;
+                        }
+                case 2:
+                    augmente:
+                    augmente_qte();
+                    printf("\n1. Augmenter la quantite d un ou plusieurs produit\n2. Reduire la quantite d un ou plusieurs produit\n3. Afficher le journal des ventes\n4. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch (choix){
+                            case 1:
+                                goto augmente;
+                            case 2:
+                                goto reduire;
+                            default:
+                                goto start1;
 
-                }
-///Gestion vente\achat
-        case 4:
+
+                        }
+                case 3:
+                    Journal_ventes();
+                    printf("\n1. Augmenter la quantite d un ou plusieurs produit\n2. Reduire la quantite d un ou plusieurs produit\n3. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch (choix){
+                            case 1:
+                                goto augmente;
+                            case 2:
+                                goto reduire;
+                            default:
+                                goto start1;
+                        }
+                default:
+                    goto start1;
+            }
 ///Etat d'inventaire
         case 5:
 ///Quit program
