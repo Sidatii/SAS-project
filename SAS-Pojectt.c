cedeb48056385f9450849a02d2b02ac2f1575be0
sidatii <sidatnouhi@gmail.com>
2022-09-19 23:50:05 +0100

    Version 0.5

--- SAS-Pojectt.c
+++ SAS-Pojectt.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <time.h>
 int nb_produit = 0;
 int compteur = 0;
+int compteur1 =0;
 
 typedef struct{
 
@@ -11,7 +13,7 @@ float price_TTC;
 
 }infos;
 
-typedef struct{
+typedef struct{ ///Produits
 
 int codee;
 infos inf;
@@ -20,23 +22,59 @@ infos inf;
 
 typedef struct{
 
+int jour;
+int mois;
+int annee;
+
+}date_operation1;
+
+typedef struct{
+
 
 char nom[14];
 int qty;
 float prix;
 float Total_TTC;
+date_operation1 d;
 
-}info;
+}informations1;
 
-typedef struct{
+typedef struct{ ///Ventes
 
 int codee;
-info inf;
+informations1 inf;
 
 }ventes;
 
+typedef struct{
+
+int jour;
+int mois;
+int annee;
+
+}date_operation2;
+
+typedef struct{
+
+
+char nom[14];
+int qty;
+float prix_TTC;
+float Total;
+date_operation2 dt;
+
+}informations2;
+
+typedef struct{ ///Achats
+
+int codee;
+informations2 info;
+
+}achats;
+
 products s[100];
 ventes t[100];
+achats a[100];
 int j, n,choix;
 char check[100];
 int i,j;
@@ -105,80 +143,96 @@ for (j=0; j<nb_produit;j++)
 
 void list_produits(){
 
+    printf("Code\tNom\tQte\tPrix HT\t        Prix TTC\n");
     for (j=0; j<nb_produit;j++){
     printf("\n%d \t %s \t %d \t %f \t %f\n",s[j].codee,s[j].inf.nom,s[j].inf.qty,s[j].inf.price,s[j].inf.price_TTC);
     }
 
 }
 
-void reduire_qte(){
+void Enregistrement_ventes(){
 
-int reduire_num,reduire_code,redu_qte,index;
+int code_code,qte_vendue,index;
 int n;
-printf("************************Reduire la quantite d un ou plusieurs produits************************\n\n ");
-printf("Veuillez saisir le nombre des produits a mettre a jour: ");
+printf("************************Enregister une ou plusieurs operation vente************************\n\n ");
+printf("Veuillez saisir le nombre des produits vendus: ");
 scanf("%d", &n);
 
 for(i=0; i<n; i++){
-    printf("Veuillez inserer le code du produit a mettre a jour n %d: ",i+1);
-    scanf("%d",&reduire_code);fflush(stdin);
-    printf("Veuillez inserer la quantite a deduire: ");
-    scanf("%d",&redu_qte);fflush(stdin);
+    printf("Veuillez saisir le code du produit vendu n %d: ",i+1);
+    scanf("%d",&code_code);fflush(stdin);
+    printf("Veuillez saisir la quantite vendue: ");
+    scanf("%d",&qte_vendue);fflush(stdin);
 
         for(int m=0; m<nb_produit;m++)
-            if(s[m].codee==reduire_code){
+            if(s[m].codee==code_code){
                 index=m;
             }
-        s[index].inf.qty -= redu_qte;
+        s[index].inf.qty -= qte_vendue;
         t[compteur].codee = s[index].codee;
         strcpy(t[compteur].inf.nom,s[index].inf.nom);
-        t[compteur].inf.qty = redu_qte;
+        t[compteur].inf.qty = qte_vendue;
         t[compteur].inf.prix = s[index].inf.price;
-        t[compteur].inf.Total_TTC = s[compteur].inf.price*s[compteur].inf.qty*1.15;
+        t[compteur].inf.Total_TTC = s[index].inf.price*qte_vendue*1.15;
+        time_t h = time(NULL);
+        struct tm tm = *localtime(&h);
+        t[compteur].inf.d.jour = tm.tm_mday;
+        t[compteur].inf.d.mois = tm.tm_mon + 1;
+        t[compteur].inf.d.annee = tm.tm_year + 1900;
         compteur++;
 
 
 
-    printf("..................La quantite produit est mis a jour..................\n\n");
+    printf("..................L'operation est enregistree..................\n\n");
 
 }
-printf("Votre nouvelle liste s'affiche comme suit: \n\n");
-printf("Code\tNom\tQte\tPrix HT\t  Prix TTC");
-for (int k=0; k<nb_produit;k++)
-    printf("\n%d \t %s \t %d \t %f \t %f\n",s[k].codee,s[k].inf.nom,s[k].inf.qty,s[k].inf.price,s[k].inf.price_TTC);
+printf("..................journal des ventes..................\n\n");
+printf("Code\tNom\tQte\tPrix HT\t        Total TTC\t        Date operation");
+for (int k=0; k<compteur;k++)
+    printf("\n%d\t%s\t%d\t%f\t%f\t%d/%d/%d\n",t[k].codee,t[k].inf.nom,t[k].inf.qty,t[k].inf.prix,t[k].inf.Total_TTC,t[k].inf.d.jour,t[k].inf.d.mois,t[k].inf.d.annee);
 
 }
 
-void augmente_qte(){
+void Enregistrement_achats(){
 
-int augmente_num,augmente_code,augm_qte,index;
+int qte_code2,qte_achetee,index;
 int n;
-printf("************************Alimenter la quantite d un ou plusieurs produits************************\n\n ");
-printf("Veuillez saisir le nombre des produits a mettre a jour: ");
+printf("************************Alimenter le stock d un ou plusieurs produits************************\n\n ");
+printf("Veuillez saisir le nombre des produits a alimenter: ");
 scanf("%d", &n);
 
 for(i=0; i<n; i++){
-    printf("Veuillez inserer le code du produit a mettre a jour n %d: ",i+1);
-    scanf("%d",&augmente_code);fflush(stdin);
-    printf("Veuillez inserer la quantite a deduire: ");
-    scanf("%d",&augm_qte);fflush(stdin);
+    printf("Veuillez saisir le code du produit a alimenter n %d: ",i+1);
+    scanf("%d",&qte_code2);fflush(stdin);
+    printf("Veuillez saisir la quantite a ajouter: ");
+    scanf("%d",&qte_achetee);fflush(stdin);
     for (j=0; j<nb_produit;j++){
 
         for(int m=0; m<nb_produit;m++){
-            if(s[m].codee==augmente_code){
+            if(s[m].codee==qte_code2){
                 index=m;
             }
-                s[index].inf.qty += augm_qte;
-
+        s[index].inf.qty += qte_achetee;
+        a[compteur1].codee = s[index].codee;
+        strcpy(a[compteur1].info.nom,s[index].inf.nom);
+        a[compteur1].info.qty = qte_achetee;
+        a[compteur1].info.prix_TTC = s[index].inf.price*1.15;
+        a[compteur1].info.Total = s[index].inf.price_TTC*qte_achetee;
+        time_t h2 = time(NULL);
+        struct tm tm = *localtime(&h2);
+        a[compteur1].info.dt.jour=tm.tm_mday;
+        a[compteur1].info.dt.mois=tm.tm_mon + 1;
+        a[compteur1].info.dt.annee=tm.tm_year + 1900;
+        compteur1++;
         }
     }
-    printf("..................Votre quantite produit est mis a jour..................\n\n");
+    printf("..................L'operation est enregistree.................\n\n");
 
 }
-printf("Votre nouvelle liste s'affiche comme suit: \n\n");
-printf("Code\tNom\tQte\tPrix HT\t  Prix TTC");
-for (int k=0; k<nb_produit;k++)
-    printf("\n%d \t %s \t %d \t %f \t %f\n",s[k].codee,s[k].inf.nom,s[k].inf.qty,s[k].inf.price,s[k].inf.price_TTC);
+printf("..................journal des achats..................\n\n");
+printf("Code\tNom\tQte\tPrix HT\t        Prix TTC\t        Date operation\n");
+for (int k=0; k<compteur1;k++)
+    printf("\n%d\t%s\t%d\t%f\t%f\t%d/%d/%d\n",a[k].codee,a[k].info.nom,a[k].info.qty,a[k].info.prix_TTC,a[k].info.Total,a[k].info.dt.jour,a[k].info.dt.mois,a[k].info.dt.annee);
 
 }
 
@@ -217,40 +271,50 @@ for (int k=0; k<nb_produit;k++)
 }
 
 void Journal_ventes(){
-    printf("\nCode\tNom\tQte\tPrix\tTotal TTC\n");
+    printf("\nCode\tNom\tQte\tPrix\t   Total TTC\t   Date operation\n");
     for (j=0; j<compteur;j++){
-    printf("\n%d \t %s \t %d \t %f \t %f\n",t[j].codee,t[j].inf.nom,t[j].inf.qty,t[j].inf.prix,t[j].inf.Total_TTC);
+    printf("\n%d\t%s\t%d\t%f\t%f\t%d/%d/%d\n",t[j].codee,t[j].inf.nom,t[j].inf.qty,t[j].inf.prix,t[j].inf.Total_TTC,t[j].inf.d.jour,t[j].inf.d.mois,t[j].inf.d.annee);
     }
 
 }
 
+void Journal_achats(){
+    printf("\nCode\tNom\tQte\tPrix TTC\t     Total\t     Date operation\n");
+    for (i=0; i<compteur1;i++){
+    printf("\n%d\t%s\t%d\t%f\t%f\t%d/%d/%d\n",a[i].codee,a[i].info.nom,a[i].info.qty,a[i].info.prix_TTC,a[i].info.Total,a[i].info.dt.jour,a[i].info.dt.mois,a[i].info.dt.annee);
+    }
 
+}
 
 int main () {
 
     start1:
-    system("cls");
     printf("*******************************************************************************************************************\n");
     printf("                           ||      The NOUHI Pharmacy management system         ||\n");
     printf("                           ||  Your application to better manage your pharmacy  ||\n");
     printf("*******************************************************************************************************************\n\n");
-    printf("Bienvenue!! Que est ce que vous voulez faire?\n\n");
+    printf("\033[0;36mBienvenue!! Que est ce que vous voulez faire?\033[0m\n\n");
     invalid:
-    printf("________________________________\n");
-    printf("1 : Ajouter un ou plusieurs produits \n");
-    printf("2 : Supprimer un ou plusieurs produits\n");
-    printf("3 : Liste des produits \n");
-    printf("4 : Gestion vente/achat \n");
-    printf("5 : Etat d'inventaire \n");
-    printf("6 : Quitter le program\n");
-    printf("________________________________\n");
+    printf("|********************************************|\n");
+    printf("|1 : Ajouter un ou plusieurs produits        |\n");
+    printf("|--------------------------------------------|\n");
+    printf("|2 : Supprimer un ou plusieurs produits      |\n");
+    printf("|--------------------------------------------|\n");
+    printf("|3 : Liste des produits                      |\n");
+    printf("|--------------------------------------------|\n");
+    printf("|4 : Gestion vente/achat                     |\n");
+    printf("|--------------------------------------------|\n");
+    printf("|5 : Etat d'inventaire                       |\n");
+    printf("|--------------------------------------------|\n");
+    printf("|6 : Quitter le program                      |\n");
+    printf("|********************************************|\n");
     printf("Veuillez saisir votre choix: ");
     scanf("%d",&choix);
 
     while ((choix < 1 || choix > 6) ||  (sscanf(check, "%d", &choix)==1)){
         fgets(check, 100, stdin);
         system("cls");
-        printf("Saisie invalid!!! \nVeuillez saisir un choix valid.\n ");
+        printf("\a\033[0;41mSaisie invalid!!!\033[0m \nVeuillez saisir un choix valid.\n");
         goto invalid;
         }
 
@@ -359,57 +423,75 @@ int main () {
             }
 ///Gestion vente\achat
         case 4:
-            printf("\n1. Reduire la quantite d un ou plusieurs produit\n2. Alimenter la quantite d un ou plusieurs produit\n3. Retour au menu principale\n");
+            printf("\n1. Enregistrer une ou plusieurs operations vente\n2. Alimenter le stock d un ou plusieurs produit\n3. Afficher le journal des ventes\n4. Afficher le journal des achats\n5. Retour au menu principale\n");
             printf("-------> ");
             scanf("%d",&choix);
             system("cls");
             switch(choix){
+
+                ///Enregistrement des ventes
                 case 1:
-                    reduire:
-                    reduire_qte();
-                    printf("\n1. Reduire la quantite d un ou plusieurs produit\n2. Augmenter la quantite d un ou plusieurs produit\n3. Retour au menu principale\n");
+                    ventes:
+                    Enregistrement_ventes();
+                    printf("\n1. Enregistrer une ou plusieurs operations vente\n2. Afficher le journal des ventes\n4. Retour au menu principale\n");
                     printf("-------> ");
                     scanf("%d",&choix);
                     system("cls");
                         switch (choix){
                             case 1:
-                                goto reduire;
+                                goto ventes;
                             case 2:
-                                goto augmente;
+                                goto journal_vte;
                             default:
                                 goto start1;
                         }
+                ///Enregistrement des achats
                 case 2:
-                    augmente:
-                    augmente_qte();
-                    printf("\n1. Augmenter la quantite d un ou plusieurs produit\n2. Reduire la quantite d un ou plusieurs produit\n3. Afficher le journal des ventes\n4. Retour au menu principale\n");
+                    achats:
+                    Enregistrement_achats();
+                    printf("\n1. Alimenter le stock d un ou plusieurs produit\n2. Afficher le journal des achats\n3. Retour au menu principale\n");
                     printf("-------> ");
                     scanf("%d",&choix);
                     system("cls");
                         switch (choix){
                             case 1:
-                                goto augmente;
+                                goto achats;
                             case 2:
-                                goto reduire;
+                                goto journal_ach;
                             default:
                                 goto start1;
 
 
                         }
+                ///Journal des ventes
                 case 3:
+                    journal_vte:
                     Journal_ventes();
-                    printf("\n1. Augmenter la quantite d un ou plusieurs produit\n2. Reduire la quantite d un ou plusieurs produit\n3. Retour au menu principale\n");
+                    printf("\n1. Enregistrer une ou plusieurs operations vente\n2. Retour au menu principale\n");
                     printf("-------> ");
                     scanf("%d",&choix);
                     system("cls");
                         switch (choix){
                             case 1:
-                                goto augmente;
-                            case 2:
-                                goto reduire;
+                                goto ventes;
                             default:
                                 goto start1;
                         }
+                ///Journal des achats
+                case 4:
+                    journal_ach:
+                    Journal_achats();
+                    printf("\n1. Alimenter le stock d un ou plusieurs produit\n2. Retour au menu principale\n");
+                    printf("-------> ");
+                    scanf("%d",&choix);
+                    system("cls");
+                        switch (choix){
+                            case 1:
+                                goto achats;
+                            default:
+                                goto start1;
+                        }
+                ///Menu principale
                 default:
                     goto start1;
             }
@@ -420,16 +502,6 @@ int main () {
             system("exit");
 
     }
-
-
-
-
-
-
-
-
-
-
 
 return 0;
 
